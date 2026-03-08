0c4fdacfb215cc127d232be5c3685e21615b48e7
0xgsvs <term@0xgsvs.dev>
2026-03-08 23:01:52 +0530

    feat: add 2 autocmds
    .treesitter start for installed languages
    .create dir if not present while saving the file

--- lua/nvchad/autocmds.lua
+++ lua/nvchad/autocmds.lua
@@ -85,3 +85,23 @@ autocmd("FileType", {
     end)
   end,
 })
+
+-- start treesitter for installed languages
+autocmd("BufRead", {
+  pattern = "*",
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
+
+-- create dir if not present
+autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+  callback = function(event)
+    if event.match:match "^%w%w+:[\\/][\\/]" then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
